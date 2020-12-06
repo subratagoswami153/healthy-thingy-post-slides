fef05750e6cf6dd0b03dffae587e916ecb1e087c
subrata153 <subrata.goswami153@gmail.com>
2020-12-06 13:25:22 +0530

    updated version 1.6.1

--- healthy-thingy-post-slides.php
+++ healthy-thingy-post-slides.php
@@ -17,7 +17,7 @@
  * Plugin Name:       Healthy thingy post slides
  * Plugin URI:        https://healthythingy.com/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.6.0
+ * Version:           1.6.1
  * Author:            Elmi Media
  * Author URI:        https://healthythingy.com/
  * License:           GPL-2.0+
@@ -42,7 +42,7 @@ if ( ! defined( 'PU_PLUGIN_FILE' ) ) {
  * Start at version 1.0.0
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'HEALTHY_THINGY_POST_SLIDES_VERSION', '1.6.0' );
+define( 'HEALTHY_THINGY_POST_SLIDES_VERSION', '1.6.1' );
 define('UTM_JSON',plugin_dir_path(__FILE__) . 'admin/json/utm.json');
 
 /**
--- public/js/healthy-thingy-post-slides-public.js
+++ public/js/healthy-thingy-post-slides-public.js
@@ -111,7 +111,7 @@
     }
 
     jQuery(document).ready(function () {
-        alert('version 1.6.0');
+        alert('version 1.6.1');
 
         if (healthyThingyObj.is_mobile == '1' && healthyThingyObj.utm_exists == '1') {
             if (healthyThingyObj.post_style_mobile == 'long-form-scroll') {
