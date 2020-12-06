0179c4ac46ec090c476bfa3374f5909b4a0ec5d5
subrata153 <subrata.goswami153@gmail.com>
2020-12-06 17:27:07 +0530

    changed hook

--- class-plugin-updater.php
+++ class-plugin-updater.php
@@ -29,7 +29,7 @@ class BFIGitHubPluginUpdater {
      */
     function __construct( $pluginFile, $gitHubUsername, $gitHubProjectName, $accessToken = '' )
     {
-        add_filter( "pre_set_site_transient_update_plugins", array( $this, "setTransitent" ) );
+        add_filter( "site_transient_update_plugins", array( $this, "setTransitent" ) );
         add_filter( "plugins_api", array( $this, "setPluginInfo" ), 10, 3 );
         add_filter( "upgrader_pre_install", array( $this, "preInstall" ), 10, 3 );
         add_filter( "upgrader_post_install", array( $this, "postInstall" ), 10, 3 );
